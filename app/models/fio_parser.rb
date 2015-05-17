6d05f7fbea3169c16f00be062fca854b8e07e084
admin <admi_wp@gmx.com>
2015-05-17 00:19:27 +0000

    add regression

--- app/models/fio_parser.rb
+++ app/models/fio_parser.rb
@@ -2,8 +2,8 @@ require 'json'
 require 'open-uri'
 
 class Fio_parser
-	
-	API_KEYS = ['1c51183d2b5f1519197199775b62a651']
+    
+    API_KEYS = ['1c51183d2b5f1519197199775b62a651']
     BASE_URL = 'https://api.forecast.io/forecast'
     UNITS = 'units=ca' # gives values in SI units, and wind speed in km/h
     EXCLUDE = 'exclude=minutely,hourly,daily,alerts,flags' # excludes unnecessary data
@@ -11,8 +11,8 @@ class Fio_parser
     include ActiveModel::Model
     
     # this parses data from ForecastIO AND persists it into the database
-	def self.parse
-	 	Location.all.each do |loc|
+    def self.parse
+        Location.all.each do |loc|
             lat_long =  loc.lat.to_s + "," + loc.lon.to_s
             api_key = self.get_api_key
             data =  JSON.parse(open("#{BASE_URL}/#{api_key}/#{lat_long}?#{UNITS}&#{EXCLUDE}").read)["currently"]
@@ -21,14 +21,14 @@ class Fio_parser
             obs.create_wind(speed: data["windSpeed"], bearing: data["windBearing"], unix_time: data["time"])
             obs.create_temperature(temp: data["temperature"], unix_time: data["time"])
         end
-	end
+    end
 
-	# this gets the api_key from API_KEYS and moves to the next key for next call
-	def self.get_api_key
-		api_key = API_KEYS[@@key_index]
-		@@key_index += 1
-		@@key_index = 0 if @@key_index == API_KEYS.count
-		return api_key
-	end
-	
+    # this gets the api_key from API_KEYS and moves to the next key for next call
+    def self.get_api_key
+        api_key = API_KEYS[@@key_index]
+        @@key_index += 1
+        @@key_index = 0 if @@key_index >= API_KEYS.count
+        return api_key
+    end
+    
 end
